e2686e26f8ace433159375d2bcc390daa5c844f0
U170709059 <gizemkurnaz@posta.mu.edu.tr>
2020-10-16 23:08:32 +0300

    Initial commit

---
+++ app/src/main/java/com/example/listviewexample/CustomAdapterActivity.java
@@ -0,0 +1,49 @@
+package com.example.listviewexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CustomAdapterActivity extends AppCompatActivity {
+
+    final List<Animal> animals = new ArrayList<Animal>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom_adapter);
+
+        animals.add(new Animal("Ant", R.mipmap.ant));
+        animals.add(new Animal("Bear",R.mipmap.bear));
+        animals.add(new Animal("Bird",R.mipmap.bird));
+        animals.add(new Animal("Cat",R.mipmap.cat));
+        animals.add(new Animal("Dog",R.mipmap.dog));
+        animals.add(new Animal("Donkey",R.mipmap.donkey));
+        animals.add(new Animal("Elephant",R.mipmap.elephant));
+        animals.add(new Animal("Giraffe",R.mipmap.giraffe));
+        animals.add(new Animal("Goat",R.mipmap.goat));
+        animals.add(new Animal("Monkey",R.mipmap.monkey));
+        animals.add(new Animal("Pig",R.mipmap.pig));
+        animals.add(new Animal("Rat",R.mipmap.rat));
+        animals.add(new Animal("Snake",R.mipmap.snake));
+        animals.add(new Animal("Spider",R.mipmap.spider));
+
+
+
+        final ListView listView = findViewById(R.id.listview);
+        AnimalAdapter adapter = new AnimalAdapter(this,animals);
+
+        listView.setAdapter(adapter);
+
+    }
+}
+
+
+
+
+
+
